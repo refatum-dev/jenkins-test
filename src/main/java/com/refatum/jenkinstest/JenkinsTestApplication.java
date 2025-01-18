41e3bfbe09ff75d62d604cf86dfbde4656a076c8
refatum-dev <refatum.dev@gmail.com>
2025-01-18 08:31:54 +0100

    test

--- src/main/java/com/refatum/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/refatum/jenkinstest/JenkinsTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
 
 }
